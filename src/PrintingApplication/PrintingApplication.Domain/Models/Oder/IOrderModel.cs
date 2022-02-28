1bee9dcc2ece116d3c151368675f72fe7ef1f066
Risal Walangadi <risal.walangadi@gmail.com>
2022-02-28 22:21:18 +0800

    add order model

---
+++ src/PrintingApplication/PrintingApplication.Domain/Models/Oder/IOrderModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PrintingApplication.Domain.Models.Oder
+{
+    public interface IOrderModel : IModel
+    {
+        uint pelanggan_id { get; set; }
+        string keterangan { get; set; }
+        DateTime tanggal { get; set; }
+        decimal sub_total { get; set; }
+        decimal total_diskon { get; set; }
+        decimal total { get; set; }
+        decimal bayar { get; set; }
+        decimal kembali { get; set; }
+    }
+}
---
+++ src/PrintingApplication/PrintingApplication.Domain/Models/Oder/OrderModel.cs
@@ -0,0 +1,70 @@
+﻿using PrintingApplication.Domain.Models.Pelanggan;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PrintingApplication.Domain.Models.Oder
+{
+    public class OrderModel : IOrderModel
+    {
+        [Browsable(false)]
+        [Display(Name = "ID")]
+        public uint id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public IPelangganModel Pelanggan { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        private uint _pelanggan_id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        [Browsable(false)]
+        [Range(1, uint.MaxValue, ErrorMessage = "Pelanggan harus diisi !!!")]
+        [Display(Name = "Pelanggan ID")]
+        public uint pelanggan_id
+        {
+            get { return Pelanggan.id != default(uint) ? Pelanggan.id : _pelanggan_id; }
+            set { _pelanggan_id = value; }
+        }
+
+        [DefaultValue("")]
+        [StringLength(255, ErrorMessage = "Panjang maksimal keterangan 255 karakter !!!")]
+        [Display(Name = "Keterangan")]
+        public string keterangan { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        [DisplayFormat(DataFormatString = "dd/MM/yyyy HH:mm")]
+        [Display(Name = "Tanggal")]
+        public DateTime tanggal { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        [Browsable(false)]
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        [DefaultValue(0)]
+        [Display(Name = "Sub Total")]
+        public decimal sub_total { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        [Browsable(false)]
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        [DefaultValue(0)]
+        [Display(Name = "Total Diskon")]
+        public decimal total_diskon { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        [Browsable(false)]
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        [DefaultValue(0)]
+        [Display(Name = "Total")]
+        public decimal total { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        [Browsable(false)]
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        [DefaultValue(0)]
+        [Display(Name = "Bayar")]
+        public decimal bayar { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        [Browsable(false)]
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        [DefaultValue(0)]
+        [Display(Name = "Kembali")]
+        public decimal kembali { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public OrderModel()
+        {
+            Pelanggan = new PelangganModel();
+        }
+    }
+}
