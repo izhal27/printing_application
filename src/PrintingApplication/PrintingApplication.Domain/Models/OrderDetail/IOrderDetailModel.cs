93c859dc5829db342083045c2b4996ed3b522429
Risal Walangadi <risal.walangadi@gmail.com>
2022-02-28 22:46:03 +0800

    add order detail model

---
+++ src/PrintingApplication/PrintingApplication.Domain/Models/OrderDetail/IOrderDetailModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PrintingApplication.Domain.Models.OrderDetail
+{
+    public interface IOrderDetailModel : IModel
+    {
+        uint order_id { get; set; }
+        string jenis_orderan { get; set; }
+        decimal harga_satuan { get; set; }
+        decimal jumlah { get; set; }
+        decimal diskon { get; set; }
+        decimal sub_total { get; set; }
+    }
+}
