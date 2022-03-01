c586d7b4354267db0387a8c5e3cce08b8a539ef2
Risal Walangadi <risal.walangadi@gmail.com>
2022-03-01 17:42:13 +0800

    add model data annotation check

---
+++ src/PrintingApplication/PrintingApplication.Services/CommonServices/IModelDataAnnotationCheck.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace PrintingApplication.Services.Services
+{
+    public interface IModelDataAnnotationCheck
+    {
+        void ValidateModel<TDomainModel>(TDomainModel domainModel);
+        void ValidateModels<TDomainModel>(IEnumerable<TDomainModel> listDomainModel);
+    }
+}
---
+++ src/PrintingApplication/PrintingApplication.Services/CommonServices/ModelDataAnnotationCheck.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PrintingApplication.Services.Services
+{
+    public class ModelDataAnnotationCheck : IModelDataAnnotationCheck
+    {
+        public void ValidateModel<TDomainModel>(TDomainModel model)
+        {
+            var validationResultList = new List<ValidationResult>();
+            ValidationContext validationContext = new ValidationContext(model);
+            StringBuilder stringBuilder = new StringBuilder();
+
+            if (!Validator.TryValidateObject(instance: model,
+                                             validationContext: validationContext,
+                                             validationResults: validationResultList,
+                                             validateAllProperties: true))
+            {
+                foreach (var validationResult in validationResultList)
+                {
+                    stringBuilder.Append(validationResult.ErrorMessage)
+                                 .AppendLine();
+                }
+            }
+
+            if (validationResultList.Count > 0)
+            {
+                throw new ArgumentException(stringBuilder.ToString());
+            }
+        }
+
+        public void ValidateModels<TDomainModel>(IEnumerable<TDomainModel> models)
+        {
+            foreach (var model in models)
+            {
+                ValidateModel(model);
+            }
+        }
+    }
+}
